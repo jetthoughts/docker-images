cb46e87a34a1e2bec30d4210d35c5083db8d63d9
Michael Nikitochkin <miry.sof@gmail.com>
2018-05-13 13:42:45 +0200

    Updated Storjshare server

--- storjshare/README.md
+++ storjshare/README.md
@@ -7,3 +7,12 @@ Run daemon to share the space https://github.com/storj/storjshare-daemon
 ```shell
 $ docker build -t miry/storjshare .
 ```
+
+# Run
+
+```shell
+$ docker run -v $(pwd)/storj:/storj -t miry/storjshare create --storj $STORJ_PAYMENT_ADDRESS --key $STORJ_NETWORK_PRIVATE_KEY --maxtunnels 10 --noedit --size 10GB --storage /storj/data --logdir /storj/logs -o /storj/config.json
+$ docker run -v $(pwd)/storj:/storj -P -t miry/storjshare daemon -F
+$ docker run -v $(pwd)/storj:/storj -P -t miry/storjshare start --config /storj/config.json
+$ docker run -v $(pwd)/storj:/storj -P -t miry/storjshare status
+```
--- storjshare/run.sh
+++ storjshare/run.sh
@@ -3,11 +3,27 @@
 set -x
 set -e
 
-storjshare create --storj $STORJ_PAYMENT_ADDRESS --key $STORJ_NETWORK_PRIVATE_KEY --maxtunnels 10 \
-                  --noedit --size 10GB --storage /storj/data --logdir /storj/logs -o /storj/config.json
+CONFIG=/storj/config.json
+DATA=/storj/data
+LOGS=/storj/logs
+
+if [ ! -f "$CONFIG" ]; then
+  STORGSHARE_ARGS="--storj $STORJ_PAYMENT_ADDRESS --maxtunnels 10 --noedit --size 10GB --storage $DATA --logdir $LOGS -o $CONFIG"
+
+  if [ -n "$STORJ_NETWORK_PRIVATE_KEY" ]; then
+    STORGSHARE_ARGS="$STORGSHARE_ARGS --key $STORJ_NETWORK_PRIVATE_KEY"
+  fi
+  storjshare create $STORGSHARE_ARGS
+fi
+
+mkdir -p $DATA $LOGS
 
 storjshare daemon
 
-storjshare start -c /storj/config.json
+storjshare status
+
+storjshare start -c $CONFIG
 
 storjshare status
+
+while true; do sleep 10000; done
